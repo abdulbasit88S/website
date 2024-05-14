fa4d35dc4df215463a80d20735fcfe1271ba0ed4
abdulbasit88S <abdulbasit12345k@gmail.com>
2024-05-14 22:35:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+function toggleMenu() {
+  var dropdown = document.getElementById("menuDropdown");
+  dropdown.classList.toggle("show");
+}
+
+// Close the menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.menu-btn')) {
+      var dropdowns = document.getElementsByClassName("menu-dropdown");
+      for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
+      }
+  }
+}
